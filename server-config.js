875f26ce485ff28e37d61fcbbb74974f6b7c8e8d
David H <davidhamberlin@gmail.com>
2017-04-24 14:35:00 -0400

    set up client directories and make server serve static

--- server-config.js
+++ server-config.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const db = require('./db-config')
 
 const app = express();
 
+app.use(express.static(__dirname + '/client'));
 
-app.get('/', (req, res) => {
-  res.status(200);
-  res.send('Greetings');
-})
+// app.get('/', (req, res) => {
+//   res.status(200);
+//   res.sendFile(__dirname + 'client/index.html');
+// })
 
 module.exports = app;
--- server.js
+++ server.js
@@ -7,5 +7,3 @@ port = process.env.PORT || 8080;
 
 app.listen(port)
 console.log('listening on port ' + port)
-
-module.exports = app;
